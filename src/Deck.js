4777a83a25908a8cd2cbd4c6bf217b015a7c497d
angelilton <epifanio.rds@gmail.com>
2022-08-13 17:58:04 -0400

    add  rotate to the cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,8 @@
 import { useRef } from 'react';
-import { View, Text, PanResponder, Animated } from 'react-native';
+import { View, Text, PanResponder, Animated, Dimensions } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const Deck = ({ data }) => {
 
@@ -9,19 +10,58 @@ const Deck = ({ data }) => {
 
   const panResponder = useRef(
     PanResponder.create({
+      onStartShouldSetPanResponder: () => {},
       onMoveShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRelease: () => {},
+      onPanResponderRelease: () => {
+        //when the finger is leave
+      },
     })
   ).current;
 
+  function getLayout() {
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg'],
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{rotate}]
+    }
+  }
+
 
   return (
-    <Animated.View style={position.getLayout()} {...panResponder.panHandlers}>
-      {data.map((item) => (
-        <Card key={item.id}>
+    <View>
+      {data.map((item, index) => {
+        if (index === 0) {
+          return (
+            <Animated.View
+              key={item.id}
+              style={getLayout()}
+              {...panResponder.panHandlers}
+            >
+              <Card key={item.id}>
+                <Card.Title>{item.text}</Card.Title>
+                <Card.Image source={{ uri: item.uri }} />
+                <Text style={{ marginBottom: 10 }}>
+                  I can customize the Card further.
+                </Text>
+                <Button
+                  icon={{ name: 'code' }}
+                  backgroundColor='#03a9f3'
+                  title={' View Now!'}
+                />
+              </Card>
+            </Animated.View>
+          );
+        }
+
+        return (
+          <Card key={item.id}>
           <Card.Title>{item.text}</Card.Title>
           <Card.Image source={{ uri: item.uri }} />
           <Text style={{ marginBottom: 10 }}>
@@ -33,8 +73,9 @@ const Deck = ({ data }) => {
             title={' View Now!'}
           />
         </Card>
-      ))}
-    </Animated.View>
+        )
+      })}
+    </View>
   );};
 
 
